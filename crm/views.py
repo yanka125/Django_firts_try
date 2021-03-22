ba10cadad0dacf1c07ad99cc98656f1e292d8b2d
yanka125 <yanka7356@gmail.com>
2021-03-22 19:01:01 +0300

    Second commit

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Order
 
 # Create your views here.
+def first_page(request):
+    object_list = Order.objects.all()
+    return render(request, './index.html', { 'object_list': object_list})
+
+
+def thanks_page(request):
+    name = request.POST['name']
+    phone = request.POST['phone']
+    element = Order(order_name=name, order_phone=phone)
+    element.save()
+    return render(request, './thanks_page.html', { 'name': name,
+                                                   'phone': phone})
