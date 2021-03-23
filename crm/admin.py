78fc305e4b8c90b5098da19ba80cbf0b60ebbbf2
yanka125 <yanka7356@gmail.com>
2021-03-23 22:50:07 +0300

    9 commit

--- crm/admin.py
+++ crm/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from .models import Order, StatusCrm, ComentCrm
 
 # Register your models here.
-admin.site.register(Order)
+class Coment(admin.StackedInline):
+    model = ComentCrm
+    fields = ('coment_dt', 'coment_text')
+    readonly_fields = ('coment_dt',)
+    extra = 0
+
+class OrderAdm(admin.ModelAdmin):
+    list_display = ('id', 'order_status', 'order_name', 'order_phone', 'order_dt')
+    list_display_links = ('id', 'order_name')
+    search_fields = ('id', 'order_name', 'order_phone', 'order_dt')
+    list_filter = ('order_status',)
+    list_editable = ( 'order_status', 'order_phone')
+    list_per_page = 10
+    list_max_show_all = 100
+    fields = ('id', 'order_status', 'order_dt', 'order_name', 'order_phone')
+    readonly_fields = ('id', 'order_dt')
+    #Поле класса coment
+    inlines = [Coment,]
+
+admin.site.register(Order, OrderAdm)
 admin.site.register(StatusCrm)
 admin.site.register(ComentCrm)
--- telebot/sendmessage.py
+++ telebot/sendmessage.py
@@ -3,19 +3,42 @@ from .models import TeleSettings
 
 
 def sendTelegram(tg_name, tg_phone):
-    settings = TeleSettings.objects.get(pk=1)
-    token = str(settings.tg_token)
-    chat_id = str(settings.tg_chat)
-    text = str(settings.tg_message)
-    api = 'https://api.telegram.org/bot'
-    method = api + token + '/sendmessage'
+    if TeleSettings.objects.get(pk=1):
+        settings = TeleSettings.objects.get(pk=1)
+        token = str(settings.tg_token)
+        chat_id = str(settings.tg_chat)
+        text = str(settings.tg_message)
+        api = 'https://api.telegram.org/bot'
+        method = api + token + '/sendmessage'
 
-    part_1 = text[0:text.find('{')]
-    part_2 = text[text.find('}')+1:text.rfind('{')]
-    part_3 = text[text.rfind('}'):-1]
+        if text.find('{') and text.find('}') and text.rfind('{') and text.rfind('}'):
+            part_1 = text[0:text.find('{')]
+            part_2 = text[text.find('}')+1:text.rfind('{')]
+            part_3 = text[text.rfind('}'):-1]
 
-    text_slice = part_1 + tg_name + part_2 + tg_phone + part_3
-    req = requests.post(method, data={
-        'chat_id': chat_id,
-        'text': text_slice,
-    })
+            text_slise = part_1 + tg_name + part_2 + tg_phone + part_3
+
+        else:
+            text_slise = text
+        try:
+            req = requests.post(method, data={
+                'chat_id': chat_id,
+                'text': text_slise,
+            })
+
+        except:
+            pass
+
+        finally:
+            if req.status_code != 200:
+                print('Ошибка отправки!')
+            if req.status_code == 500:
+                print('Ошибка 500')
+            else:
+                print('Всё ОК сообщение отправлено!')
+
+
+
+    else:
+        pass
+
